8464ffc4d093e82bd1eb37035f3a91eb6b7153ef
Peter Dietz <peter@longsight.com>
2016-11-08 13:44:17 -0500

    Pingry repair index-authority to be able to reindex from PPDB
    Also, many other Pingry Authority related fixes.
    -setValue in solr as opposed to add value (multiple values)
    - disabling some authority work in discovery
    - reindex script can clean and limit, and only works with ppdb
    - can look up metadata value against ppdb to reindex

--- dspace-api/src/main/java/org/dspace/authority/indexer/AuthorityIndexClient.java
+++ dspace-api/src/main/java/org/dspace/authority/indexer/AuthorityIndexClient.java
@@ -7,6 +7,11 @@
  */
 package org.dspace.authority.indexer;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.lang.StringUtils;
 import org.dspace.authority.AuthorityValue;
 import org.apache.log4j.Logger;
 import org.dspace.content.Item;
@@ -31,26 +36,49 @@ public class AuthorityIndexClient {
     private static Logger log = Logger.getLogger(AuthorityIndexClient.class);
 
     public static void main(String[] args) throws Exception {
+        // create an options object and populate it
+        CommandLineParser parser = new PosixParser();
+        Options options = new Options();
+        options.addOption("c", "clean", false, "clean solr before reindexing");
+        options.addOption("h", "help", false, "help");
+        options.addOption("l", "limit", true, "optional limit to # to process");
+        CommandLine line = parser.parse(options, args);
 
         //Populate our solr
         Context context = new ContextNoCaching();
-        //Ensure that we can update items if we are altering our authority control
         context.turnOffAuthorisationSystem();
         ServiceManager serviceManager = getServiceManager();
-
 
         AuthorityIndexingService indexingService = serviceManager.getServiceByName(AuthorityIndexingService.class.getName(),AuthorityIndexingService.class);
         List<AuthorityIndexerInterface> indexers = serviceManager.getServicesByType(AuthorityIndexerInterface.class);
 
         if(!isConfigurationValid(indexingService, indexers)){
-                    //Cannot index, configuration not valid
             System.out.println("Cannot index authority values since the configuration isn't valid. Check dspace logs for more information.");
-
             return;
         }
-        
-        System.out.println("Retrieving all data");
+
+        if(line.hasOption('c')) {
+            log.info("Cleaning the old index");
+            System.out.println("Cleaning the old index");
+            indexingService.cleanIndex();
+        } else {
+            log.info("Not cleaning SOLR before reindex");
+        }
+
+        System.out.println("Retrieving and writing data");
         log.info("Retrieving all data");
+
+        int count=0;
+        int limit = 500;
+        if(line.hasOption('l')) {
+            if(isInteger(line.getOptionValue('l'))){
+                limit = Integer.parseInt(line.getOptionValue('l'));
+            }
+        }
+
+
+        int hit=0;
+        int miss=0;
 
         //Get all our values from the input forms
         Map<String, AuthorityValue> toIndexValues = new HashMap<>();
@@ -58,33 +86,66 @@ public class AuthorityIndexClient {
             log.info("Initialize " + indexerInterface.getClass().getName());
             System.out.println("Initialize " + indexerInterface.getClass().getName());
             indexerInterface.init(context, true);
-            while (indexerInterface.hasMore()) {
+
+
+            System.out.println(". = authority value found, _ means no match");
+
+            while (indexerInterface.hasMore() && count < limit) {
                 AuthorityValue authorityValue = indexerInterface.nextValue();
-                if(authorityValue != null){
-                    toIndexValues.put(authorityValue.getId(), authorityValue);
+
+                if(count % 100 == 0) {
+                    System.out.println("");
+                    System.out.print(count + " ");
                 }
+
+                if(authorityValue != null && StringUtils.isNotBlank(authorityValue.getValue())){
+                    toIndexValues.put(authorityValue.getId(), authorityValue);
+                    System.out.print(".");
+                    hit++;
+                } else {
+                    System.out.print("_");
+                    miss++;
+                }
+                count++;
+
             }
             //Close up
             indexerInterface.close();
         }
 
+        System.out.println("");
+        System.out.println("Count: " + count + " hit:" + hit + " miss:" + miss);
 
-        log.info("Cleaning the old index");
-        System.out.println("Cleaning the old index");
-        indexingService.cleanIndex();
-        log.info("Writing new data");
-        System.out.println("Writing new data");
-        for(String id : toIndexValues.keySet()){
+
+        log.info("Writing");
+        System.out.println("Writing");
+        int wrote=0;
+        for(String id: toIndexValues.keySet()) {
             indexingService.indexContent(toIndexValues.get(id), true);
+            context.commit();
             indexingService.commit();
+            System.out.print("+");
+            wrote++;
         }
 
-        context.commit();
+        System.out.println("");
+        System.out.println("Wrote: " + wrote);
+
         //In the end commit our server
+        context.commit();
         indexingService.commit();
         context.abort();
         System.out.println("All done !");
         log.info("All done !");
+    }
+
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
     }
 
     public static void indexItem(Context context, Item item){
